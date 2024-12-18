35d4fc56251159a396898eef70dc7cc8e1d0c24e
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-18 10:21:49 -0300

    v.1.12

--- src/Paragraph.jsx
+++ src/Paragraph.jsx
@@ -1,15 +1,15 @@
 function Paragraph(){
     return (
         <div>
-        <p>
+            <p>
             The smile of dawn arrived early May she carried a gift from her home <br></br>
             The night shed a tear to tell her of fear and of sorrow and pain she´ll never outgrow <br></br>
             Death is the first dance, eternal 
-        </p>
-        <p>
+            </p>
+            <p>
             There´s no more freedom<br></br>
             The both of you will be confined to this mind
-        </p>
+            </p>
         </div>
     )
 }
