c60cb3ef9d671dd751e5edad1815d2cc09cf2f7e
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-19 09:19:13 -0300

    v.3.7

--- src/Paragraph5.jsx
+++ src/Paragraph5.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react"
 
 function Paragraph5(){
-  const [text, setText] = useState("Stage 1")
+  const [text, setText] = useState("Type")
+  const [inputText, setInputText] = useState("")
+
+  function clickButton(){
+    setText(inputText)
+  }
+
   return (
     <div>
         <div>
-          <p style={{color:"gray"}}> {text} </p>
-          <button onClick={()=>{setText("Stage 2")}}>Change State</button>
+          <h3 style={{color:"gray"}}> {text} </h3>
+          <input value={inputText} onChange={(evento)=>{setInputText(evento.target.value)}} type="text"/>
+          <button onClick={clickButton}>Click Here To Change</button>
         </div>
     </div>
   )
