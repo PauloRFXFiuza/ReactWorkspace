2c8dad5ac63d0b440adcd91b5e0ac281053a048c
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-19 09:36:09 -0300

    v.4.2

---
+++ src/Menu.jsx
@@ -0,0 +1,11 @@
+function Menu(){
+    return(
+        <div>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </div>
+    )
+}
+
+export default Menu
